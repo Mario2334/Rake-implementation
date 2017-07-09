8047954460dd0f9e4dd58caf9903ee5a8d9fac1e
Mario2334 <sanketm221995@yahoo.co.in>
2017-07-09 13:56:34 +0530

    Rake implementation file

--- calculator.py
+++ calculator.py
@@ -1,14 +1,19 @@
-
+import csv
 from RAKE import Rake
+import email
+import os
 
-text = '''
-Anyone know a reasonable circuit for programming PALs?  I am interested
-in programming a wide range of EPLDs but would be happy with something 
-that could handle a 22V10 or thereabouts.
+base = os.getcwd() + '/Data_set'
+topic_identifier_instance = Rake('Stop_list.txt')
 
-Thanks in advance,
---Tim
-'''
-
-
-print(Rake('Stop_list.txt').run(text))
+for folder in os.listdir(base + '/Data_set'):
+    fold = base + '/Data_set/{}'.format(folder)
+    for file in os.listdir(fold):
+        mail = email.message_from_file(open(fold + '/{}'.format(file)))
+        message_string = mail.get_payload()
+        score_table = topic_identifier_instance.run(message_string)
+        score_table.insert(0, ('topic', 'word_score'))
+        print(score_table)
+        writer = csv.writer(open(base + '/Result_data_set' + '/' + folder + '/' + file + '.csv', 'w+'))
+        for row in score_table:
+            writer.writerow(row)
